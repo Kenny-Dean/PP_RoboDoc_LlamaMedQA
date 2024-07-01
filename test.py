fb23d56beb3bfe98f328abb27f3e53ff7311c890
Kenny-Dean <158468143+Kenny-Dean@users.noreply.github.com>
2024-07-02 01:22:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import torch
 
-print(torch.cuda.is_available())
-print(torch.cuda.device_count())
-print(torch.cuda.current_device())
-print(torch.cuda.device(0))
-print(torch.cuda.get_device_name(0))
+print(f"CUDA available: {torch.cuda.is_available()}")
+print(f"Device name: {torch.cuda.get_device_name(0)}")
+print(f"CUDA version: {torch.version.cuda}")
+print(f"PyTorch version: {torch.__version__}")
