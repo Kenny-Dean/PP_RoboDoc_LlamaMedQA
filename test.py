67979e29917e4ddc7912c99498c4595213640496
Kenny-Dean <158468143+Kenny-Dean@users.noreply.github.com>
2024-07-02 11:08:48 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-print(f"CUDA available: {torch.cuda.is_available()}")
-print(f"Device name: {torch.cuda.get_device_name(0)}")
-print(f"CUDA version: {torch.version.cuda}")
-print(f"PyTorch version: {torch.__version__}")
