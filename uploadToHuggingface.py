3e3abb4b480f518c726db18209023710536d6eb0
Kenny-Dean <158468143+Kenny-Dean@users.noreply.github.com>
2024-07-02 22:38:00 +0200

    update to savetoke

--- uploadToHuggingface.py
+++ uploadToHuggingface.py
@@ -1,29 +1,11 @@
-import subprocess
-import os
+from transformers import AutoTokenizer
 
-# Definiere den Pfad zu deinem Modellverzeichnis und das Repository-URL
-model_dir = "/home/pp/RoboDoc/PP_RoboDoc_LlamaMedQA/Llama3_8B_bnb_4bit_RoboDoc_MedQA"
-repo_url = "https://huggingface.co/KennyDain/Llama3_8B_bnb_RoboDoc_MedQA"
+# Angenommen, du hast den Tokenizer aus dem Checkpoint geladen
+checkpoint_path = "./outputs"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint_path)
 
-# Wechsle in das Modellverzeichnis
-os.chdir(model_dir)
+# Definiere den Pfad, wo du den Tokenizer speichern möchtest
+save_path = "./Llama3_8B_bnb_4bit_RoboDoc_MedQA_Tokenizer"
 
-# Überprüfen, ob der Remote-Name 'origin' existiert
-result = subprocess.run(["git", "remote"], capture_output=True, text=True)
-if 'origin' in result.stdout:
-    subprocess.run(["git", "remote", "set-url", "origin", repo_url], check=True)
-else:
-    subprocess.run(["git", "remote", "add", "origin", repo_url], check=True)
-
-# Git LFS installieren und tracken
-subprocess.run(["git", "lfs", "install"], check=True)
-subprocess.run(["git", "lfs", "track", "adapter_model.safetensors"], check=True)
-
-# Füge alle Dateien hinzu
-subprocess.run(["git", "add", "."], check=True)
-
-# Commite die Dateien
-subprocess.run(["git", "commit", "-m", "Upload fine-tuned model"], check=True)
-
-# Pushe die Dateien zum Hugging Face Model Hub
-subprocess.run(["git", "push", "origin", "master"], check=True)
+# Speichere den Tokenizer auf der Festplatte
+tokenizer.save_pretrained(save_path)
